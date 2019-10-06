0e5a565f9205f21fa97300a7d2a36793c4abdd0b
JCode1986 <joseph.hangarter@yahoo.com>
2019-10-05 23:53:07 -0700

    still need functionality to work

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,15 @@
 import {createStore, combineReducers} from 'redux';
 
 import toDoReducer from './todo.store.js';
+import itemReducer from './item.store.js';
+import listReducer from './list.store.js';
+import formReducer from './form.store.js';
 
 let reducers = combineReducers({
   toDo: toDoReducer,
+  item: itemReducer,
+  list: listReducer,
+  form: formReducer
 })
 
 export default () => createStore(reducers);
